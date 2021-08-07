ceb917a0d2abe359e430171102bb4472a519fa12
bipin9235 <87981816+bipin9235@users.noreply.github.com>
2021-08-07 14:39:19 +0530

    Update RoomBillComponent.java

--- RoomBillComponent.java
+++ RoomBillComponent.java
@@ -1,3 +1,11 @@
+/*
+#Task-21: NATUREVALLEY homestay provides convenient and comfort stay for visitors. The resort wants to automate
+its billing process. Homestay has three different types of rooms "Standard", "Deluxe" and "Cottage". They
+provide accommodation for minimum one day and maximum of 15 days.
+
+Registration ID: JIRSS1132
+Name: Bipin Kumar
+*/
 
 public interface RoomBillComponent {
 	double tax=12;
